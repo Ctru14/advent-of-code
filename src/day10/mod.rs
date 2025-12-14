f4eeea2db8e2705d700355abe2177cb94a119f38
Connor Truono <ctru14@gmail.com>
2025-12-14 07:06:56 -0800

    Gauss-Jordan elimination on matrix-defined joltage problem

--- src/day10/mod.rs
+++ src/day10/mod.rs
@@ -12,7 +12,10 @@ pub(crate) fn solve_day10() {
         indicator_lights_sum += min_buttons;
     }
 
-    println!("Indicator lights: Sum of number of hits: {}", indicator_lights_sum);
+    println!(
+        "Indicator lights: Sum of number of hits: {}",
+        indicator_lights_sum
+    );
 
     // Part 2: Increase Joltage
     let mut joltage_sum: usize = 0;
@@ -20,7 +23,7 @@ pub(crate) fn solve_day10() {
         let min_buttons = machine.solve_joltage();
         joltage_sum += min_buttons;
     }
-    
+
     println!("Joltage: Sum of number of hits: {}", joltage_sum);
 }
 
@@ -35,13 +38,56 @@ impl Machine {
     /// Return the lowest number of presses of groups of buttons required to
     /// increase the joltage to the desired amounts
     fn solve_joltage(&self) -> usize {
+        // Turn the buttons and desired joltage into a matrix
+        let mut mat = self.get_joltage_matrix();
+
+        mat.gauss_jordan_elimination();
+
+        0
+    }
+
+    /// Creates a matrix of the button presses and the desired joltage
+    /// [.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+    ///         a    b    c    d     e     f
+    /// a b c d e f  =
+    /// 0 0 0 0 1 1  3
+    /// 0 1 0 0 0 1  5
+    /// 0 0 1 1 1 0  4
+    /// 1 1 0 1 0 0  7
+    fn get_joltage_matrix(&self) -> Matrix {
+        let rows = self.joltage.len();
+        let num_buttons = self.buttons.len();
+        let cols = num_buttons + 1; // Each button is a column + the output joltage
+        let mut mat: Vec<Vec<i32>> = Vec::with_capacity(rows);
+
+        for r in 0..rows {
+            let mut row: Vec<i32> = Vec::with_capacity(cols);
+            for button in 0..num_buttons {
+                if self.buttons[button].contains(&r) {
+                    row.push(1);
+                } else {
+                    row.push(0);
+                }
+            }
+            row.push(self.joltage[r] as i32);
+            mat.push(row);
+        }
+
+        Matrix(mat)
+    }
+
+    /// Return the lowest number of presses of groups of buttons required to
+    /// increase the joltage to the desired amounts
+    /// Checks all possible values starting from max of the joltage values
+    /// Will never complete before the heat death of the universe
+    fn _solve_joltage_brute_force(&self) -> usize {
         let mut count = 0;
 
         // Keep checking all possible combinations of number of buttons pressed
         // Start with the max of the joltage numbers, the answer will never be less
         let mut presses = *self.joltage.iter().max().unwrap();
         loop {
-            if self.check_joltage_counters(presses) {
+            if self._check_joltage_counters_brute_force(presses) {
                 count = presses;
                 break;
             }
@@ -52,12 +98,10 @@ impl Machine {
     }
 
     /// Check if the joltage levels can be achieved by the number of presses
-    fn check_joltage_counters(&self, presses: usize) -> bool {
-        println!("n: {}, presses: {}", self.buttons.len(), presses);
-        for button_sets in SelectNCountIter::new(self.buttons.len(), presses) {
+    fn _check_joltage_counters_brute_force(&self, presses: usize) -> bool {
+        for button_sets in _SelectNCountIter::_new(self.buttons.len(), presses) {
             // Index of each button_sets corresponds to index of each button to press,
             // value corresponds to how many times to press it
-            // println!("Button Sets: {:?}", button_sets);
             let mut joltage_test = vec![0; self.joltage.len()];
             // Each iteration is +1 button press
             for button_set_to_press in button_sets {
@@ -73,7 +117,6 @@ impl Machine {
         }
 
         false
-        
     }
 
     /// Counts the lowest number of presses of groups of its buttons required to
@@ -117,22 +160,183 @@ impl Machine {
     }
 }
 
-struct SelectNCountIter {
+#[derive(Debug, PartialEq, Eq, Clone)]
+struct Matrix(Vec<Vec<i32>>);
+
+impl Matrix {
+    /// Performs Gauss-Jordan elimination to reduce a matrix
+    fn gauss_jordan_elimination(&mut self) {
+        let rows = self.0.len();
+
+        // Current index of the column which is being transformed into a 1
+        let mut pivot_row: usize;
+        let mut pivot_col: usize = 0;
+
+        let mut rescale_needed: bool = false;
+
+        for r in 0..rows {
+            // 1. Find the next available pivot column
+            // Check each column for the presence of a nonzero value
+            (pivot_row, pivot_col) = match self.next_available_pivot_column(r, pivot_col) {
+                Some((row, col)) => (row, col),
+                None => {
+                    // Unable to find another pivot value. Ending
+                    return;
+                }
+            };
+
+            // 2. Swap a row with said column index present into the current row
+            self.0.swap(r, pivot_row);
+            // Now the row with the pivot column is at index r
+
+            // 3. Scale the row if needed so the pivot column entry is equal to 1
+            let rescale_result = Self::try_rescale_row(&mut self.0[r]);
+
+            // 4. Add/Subtract this row from any following rows which also contain our pivot column
+            // Only attempt this subtraction if the previous rescale attempt passed 
+            // so the pivot column value will be 1
+            if rescale_result.is_ok() {
+                self.subtract_out_pivot(r, pivot_col);
+            }
+
+            // Attempt to rescale any previously failed row
+            if rescale_needed {
+                rescale_needed = self.try_rescale_and_subtract_first_n_rows(r).is_err();
+            }
+
+            // Try a full rescale on next attempt if this row failed
+            if rescale_result.is_err() {
+                rescale_needed = true;
+            }
+        }
+    }
+
+    /// Returns the row and column index of the next available pivot column
+    /// beginning from the provided indices
+    fn next_available_pivot_column(
+        &self,
+        start_row: usize,
+        start_col: usize,
+    ) -> Option<(usize, usize)> {
+        let rows = self.0.len();
+        let cols = self.0[0].len();
+
+        // Last column is the result, do not count it
+        for check_col in start_col..cols-1 {
+            for check_row in start_row..rows {
+                if self.0[check_row][check_col] != 0 {
+                    return Some((check_row, check_col));
+                }
+            }
+        }
+
+        None
+    }
+
+    /// Goes through the first so many rows and attempts to scale them
+    /// so that the first nonzero value in that row is 1
+    ///
+    /// Return: Result whether all rows are properly rescaled
+    fn try_rescale_and_subtract_first_n_rows(&mut self, n: usize) -> Result<(), ()> {
+        let mut res = Ok(());
+        for r in 0..n {
+            // Check if this row needs a rescale
+            if let Some((pivot_col, val)) = Self::get_first_nonzero_value(&self.0[r])
+                && val != 1
+            {
+                if Self::try_rescale_row(&mut self.0[r]).is_ok() {
+                    // Row rescaled! Now subtract with it
+                    self.subtract_out_pivot(r, pivot_col);
+                } else {
+                    res = Err(());
+                }
+            };
+        }
+
+        res
+    }
+
+    /// Tries to rescale the row so that the first nonzero value is 1
+    /// Row of all zeros is properly scaled and returns Ok(())
+    ///
+    /// Return: Result whether the row was properly rescaled
+    fn try_rescale_row(row: &mut Vec<i32>) -> Result<(), ()> {
+        if let Some((_idx, scale_val)) = Self::get_first_nonzero_value(row) {
+            match scale_val {
+                1 => Ok(()), // As expected
+                -1 => {
+                    // Multiply all values of the row by -1
+                    row.iter_mut().for_each(|val| *val *= -1);
+                    Ok(())
+                }
+                0 => {
+                    panic!("Rows with all 0s should fail the option check");
+                }
+                _ => {
+                    // Uhhhh we're only using integers so this could get bad
+                    if row.iter().any(|val| val % scale_val != 0) {
+                        Err(())
+                    } else {
+                        row.iter_mut().for_each(|val| *val /= scale_val);
+                        Ok(())
+                    }
+                }
+            }
+        } else {
+            // All 0s - this is properly scaled
+            Ok(())
+        }
+    }
+
+    /// Subtracts out a pivot column using the equation in the specified row
+    fn subtract_out_pivot(&mut self, pivot_row: usize, pivot_col: usize) {
+        let rows = self.0.len();
+        let cols = self.0[0].len();
+
+        // This row must be properly scaled
+        assert_eq!(self.0[pivot_row][pivot_col], 1);
+
+        for target_row in 0..rows {
+            let pivot_val = self.0[target_row][pivot_col];
+            // println!("Pivot val: {}", pivot_val);
+            if target_row != pivot_row && pivot_val != 0 {
+                for c in 0..cols {
+                    self.0[target_row][c] -= pivot_val * self.0[pivot_row][c];
+                }
+            }
+        }
+    }
+
+    /// Returns the first nonzero value in the list along with its index
+    fn get_first_nonzero_value(row: &Vec<i32>) -> Option<(usize, i32)> {
+        for (idx, &val) in row.iter().enumerate() {
+            if val != 0 {
+                return Some((idx, val));
+            }
+        }
+
+        None
+    }
+}
+
+struct _SelectNCountIter {
     n: usize,
     /// Vec length of count, where each of them correspond to the index of each selection
     /// Each value will range from 0..=n
     next: Option<Vec<usize>>,
-
 }
 
-impl SelectNCountIter {
-    fn new(n: usize, count: usize) -> Self {
+impl _SelectNCountIter {
+    fn _new(n: usize, count: usize) -> Self {
         let next: Vec<usize> = vec![0; count];
-        Self { n, next: Some(next) }
+        Self {
+            n,
+            next: Some(next),
+        }
     }
 }
 
-impl Iterator for SelectNCountIter {
+impl Iterator for _SelectNCountIter {
     type Item = Vec<usize>;
 
     /// Ex: 5 choose 3
@@ -297,6 +501,13 @@ fn parse_input(input: &str) -> Vec<Machine> {
     machines
 }
 
+fn print_matrix(Matrix(mat): &Matrix) {
+    println!("Matrix {} x {}:", mat.len(), mat[0].len());
+    for row in mat {
+        println!("{:?}", row);
+    }
+}
+
 #[cfg(test)]
 mod test {
     use super::*;
@@ -362,19 +573,68 @@ mod test {
 
     #[test]
     fn test_selectncount_iter() {
-        let select1of3iter = SelectNCountIter::new(3, 1);
-        let s13_expected = vec![
-            vec![0],
-            vec![1],
-            vec![2],
-        ];
+        let select1of3iter = _SelectNCountIter::new(3, 1);
+        let s13_expected = vec![vec![0], vec![1], vec![2]];
 
         for (idx, item) in select1of3iter.enumerate() {
             assert_eq!(s13_expected[idx], item);
         }
+    }
 
-        for item in SelectNCountIter::new(5, 3) {
-            println!("{:?}", item);
-        }
+    #[test]
+    fn test_gauss_jordan_elimination() {
+        let mut m1 = Matrix(vec![
+            vec![1, 1, 1, 0, 0, 0, 0, 28],
+            vec![0, 0, 1, 0, 0, 1, 0, 36],
+            vec![1, 1, 0, 0, 1, 0, 1, 163],
+            vec![1, 0, 0, 1, 1, 1, 0, 27],
+            vec![0, 0, 1, 0, 0, 1, 1, 180],
+            vec![0, 0, 0, 0, 1, 0, 0, 11],
+            vec![1, 1, 1, 0, 0, 0, 1, 172],
+            vec![1, 1, 0, 0, 1, 0, 0, 19],
+        ]);
+        m1.gauss_jordan_elimination();
+
+        assert_eq!(
+            m1,
+            Matrix(vec![
+                vec![1, 0, 0, 1, 0, 0, 0, 0],
+                vec![0, 1, 0, -1, 0, 0, 0, 8],
+                vec![0, 0, 1, 0, 0, 0, 0, 20],
+                vec![0, 0, 0, 0, 1, 0, 0, 11],
+                vec![0, 0, 0, 0, 0, 1, 0, 16],
+                vec![0, 0, 0, 0, 0, 0, 1, 144],
+                vec![0, 0, 0, 0, 0, 0, 0, 0,],
+                vec![0, 0, 0, 0, 0, 0, 0, 0,],
+            ])
+        );
+        println!("Matrix 1 passed!\n");
+
+        let mut m2 = Matrix(vec![
+            vec![0, 1, 1, 0, 0, 1, 13],
+            vec![0, 0, 1, 1, 1, 1, 43],
+            vec![1, 1, 0, 0, 1, 0, 33],
+            vec![1, 0, 0, 0, 1, 0, 29],
+            vec![1, 1, 1, 0, 1, 0, 40],
+            vec![1, 1, 0, 1, 0, 1, 41],
+        ]);
+        print_matrix(&m2);
+        m2.gauss_jordan_elimination();
+        print_matrix(&m2);
+
+        let m2_expected = Matrix(vec![
+            vec![1, 0, 0, 0, 0, 0, 15],
+            vec![0, 1, 0, 0, 0, 0, 4],
+            vec![0, 0, 1, 0, 0, 0, 7],
+            vec![0, 0, 0, 1, 0, 0, 20],
+            vec![0, 0, 0, 0, 1, 0, 14],
+            vec![0, 0, 0, 0, 0, 1, 2],
+        ]);
+
+        println!("M2 Expected:");
+        print_matrix(&m2_expected);
+
+        assert_eq!(m2, m2_expected);
+        println!("Matrix 2 passed!");
     }
 }
